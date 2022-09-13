656ee3166402989f46d2a36270958b4880bbd466
fakhriR233 <fakhriramadhan233@gmail.com>
2022-09-13 14:42:33 +0700

    Update, most function work

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"dumbflix_be/database"
+	"dumbflix_be/pkg/mysql"
+	"dumbflix_be/routes"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// initial DB
+	mysql.DatabaseInit()
+
+	// run migration
+	database.RunMigration()
+
+	r := mux.NewRouter()
+
+	routes.RouteInit(r.PathPrefix("/api/v1").Subrouter())
+
+	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
+
+	fmt.Println("server running localhost:5000")
+	http.ListenAndServe("localhost:5000", r)
+}
